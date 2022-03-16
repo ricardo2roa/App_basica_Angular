ba92c07297110b5ec01a4a2e959b389fe8febd44
Ricardo Roa <ricardo2roa@gmail.com>
2022-03-16 09:21:40 -0500

    Se crea componente detalle del Plato

---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Dish} from "../shared/dish";
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
+})
+export class DishdetailComponent implements OnInit {
+
+  @Input() selectedDish: Dish;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,7 +11,39 @@ const DISHES: Dish[] = [
         label: 'Hot',
         price: '4.99',
         // tslint:disable-next-line:max-line-length
-        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        comments: [
+          {
+            rating: 5,
+            comment: 'Imagine all the eatables, living in conFusion!',
+            author: 'John Lemon',
+            date: '2012-10-16T17:57:28.556094Z'
+          },
+          {
+            rating: 4,
+            comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+            author: 'Paul McVites',
+            date: '2014-09-05T17:57:28.556094Z'
+          },
+          {
+            rating: 3,
+            comment: 'Eat it, just eat it!',
+            author: 'Michael Jaikishan',
+            date: '2015-02-13T17:57:28.556094Z'
+          },
+          {
+            rating: 4,
+            comment: 'Ultimate, Reaching for the stars!',
+            author: 'Ringo Starry',
+            date: '2013-12-02T17:57:28.556094Z'
+          },
+          {
+            rating: 2,
+            comment: 'It\'s your birthday, we\'re gonna party!',
+            author: '25 Cent',
+            date: '2011-12-02T17:57:28.556094Z'
+          }
+        ]
       },
       {
         id: '1',
@@ -53,10 +85,14 @@ const DISHES: Dish[] = [
 export class MenuComponent implements OnInit {
 
   dishes: Dish[] = DISHES
-  selectedDish: Dish = DISHES[0]
+  selectedDish: Dish
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectDish(int:string){
+    this.selectedDish = DISHES[parseInt(int)];
+    console.log(this.selectedDish.comments)
+  }
 }
