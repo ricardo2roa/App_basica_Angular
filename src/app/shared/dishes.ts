ecb89202ddbae15805363f2474077638d9f97110
Ricardo Roa <ricardo2roa@gmail.com>
2022-03-18 20:37:54 -0500

    Se crea servicio dish y clase comment

--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -52,7 +52,39 @@ export const DISHES: Dish[] = [
     featured: false,
     label: '',
     price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+    comments: [
+      {
+        rating: 5,
+        comment: 'Imagine all the eatables, living in conFusion!',
+        author: 'John Lemon',
+        date: '2012-10-16T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+        author: 'Paul McVites',
+        date: '2014-09-05T17:57:28.556094Z'
+      },
+      {
+        rating: 3,
+        comment: 'Eat it, just eat it!',
+        author: 'Michael Jaikishan',
+        date: '2015-02-13T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Ultimate, Reaching for the stars!',
+        author: 'Ringo Starry',
+        date: '2013-12-02T17:57:28.556094Z'
+      },
+      {
+        rating: 2,
+        comment: 'It\'s your birthday, we\'re gonna party!',
+        author: '25 Cent',
+        date: '2011-12-02T17:57:28.556094Z'
+      }
+    ]
   },
   {
     id: '2',
@@ -62,7 +94,39 @@ export const DISHES: Dish[] = [
     featured: false,
     label: 'New',
     price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+    comments: [
+      {
+        rating: 5,
+        comment: 'Imagine all the eatables, living in conFusion!',
+        author: 'John Lemon',
+        date: '2012-10-16T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+        author: 'Paul McVites',
+        date: '2014-09-05T17:57:28.556094Z'
+      },
+      {
+        rating: 3,
+        comment: 'Eat it, just eat it!',
+        author: 'Michael Jaikishan',
+        date: '2015-02-13T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Ultimate, Reaching for the stars!',
+        author: 'Ringo Starry',
+        date: '2013-12-02T17:57:28.556094Z'
+      },
+      {
+        rating: 2,
+        comment: 'It\'s your birthday, we\'re gonna party!',
+        author: '25 Cent',
+        date: '2011-12-02T17:57:28.556094Z'
+      }
+    ]
   },
   {
     id: '3',
@@ -72,6 +136,38 @@ export const DISHES: Dish[] = [
     featured: false,
     label: '',
     price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+    comments: [
+      {
+        rating: 5,
+        comment: 'Imagine all the eatables, living in conFusion!',
+        author: 'John Lemon',
+        date: '2012-10-16T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+        author: 'Paul McVites',
+        date: '2014-09-05T17:57:28.556094Z'
+      },
+      {
+        rating: 3,
+        comment: 'Eat it, just eat it!',
+        author: 'Michael Jaikishan',
+        date: '2015-02-13T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Ultimate, Reaching for the stars!',
+        author: 'Ringo Starry',
+        date: '2013-12-02T17:57:28.556094Z'
+      },
+      {
+        rating: 2,
+        comment: 'It\'s your birthday, we\'re gonna party!',
+        author: '25 Cent',
+        date: '2011-12-02T17:57:28.556094Z'
+      }
+    ]
   }
 ];
