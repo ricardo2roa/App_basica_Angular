cbd1cea77bc2766bb822001a6bf82962ae46c217
Ricardo Roa <ricardo2roa@gmail.com>
2022-04-06 22:05:29 -0500

    Se crea formulario reactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,10 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {FormsModule} from "@angular/forms";
-
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatOptionModule} from "@angular/material/core";
 //Servicios
 import {DishService} from "./services/dish.service";
 import {PromotionService} from "./services/promotion.service";
@@ -56,6 +59,7 @@ import {AppRoutingModule} from "./app-routing/app-routing.module";
     MatToolbarModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
@@ -64,7 +68,10 @@ import {AppRoutingModule} from "./app-routing/app-routing.module";
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatOptionModule
   ],
   providers: [
     DishService,
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {feedback, ContactType} from "../shared/feedback";
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup;
+  feedback: feedback;
+  contacttype:string[] = ContactType;
+
+  @ViewChild('fform') feedbackFormDirective:HTMLFormElement;
+  constructor(private fb:FormBuilder, private cd:ChangeDetectorRef) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
+
+  }
+
+  createForm(){
+    this.feedbackForm = this.fb.group({
+      firstname:['',Validators.required],
+      lastname: ['',Validators.required],
+      telnum: [0,Validators.required],
+      email: ['',Validators.required],
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+
+  onSubmit(){
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset({
+      firstname:'',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedbackFormDirective.resetForm();
   }
 
 }
