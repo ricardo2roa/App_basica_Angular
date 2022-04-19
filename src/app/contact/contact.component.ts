6589745d1cc01d33f55926d84ede792e62344b98
Ricardo Roa <ricardo2roa@gmail.com>
2022-04-19 11:38:45 -0500

    Se crea validaciones
    Se crea validaciones con mensajes personalizados

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,27 @@ import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {feedback, ContactType} from "../shared/feedback";
 
+interface formErros{
+  firstname:string
+  lastname:string
+  telnum:string
+  email:string
+}
+
+interface validationMessages{
+  firstname:propertiesMessages
+  lastname:propertiesMessages
+  telnum:propertiesMessages
+  email:propertiesMessages
+}
+
+interface propertiesMessages{
+  required?: string
+  minlength?: string
+  maxlength?: string
+  pattern?:string
+  email?:string
+}
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -14,6 +35,36 @@ export class ContactComponent implements OnInit {
   contacttype:string[] = ContactType;
 
   @ViewChild('fform') feedbackFormDirective:HTMLFormElement;
+
+
+ formErrors:formErros = {
+    firstname:'',
+    lastname:'',
+    telnum:'',
+    email:''
+  };
+
+  validationMessages:validationMessages={
+    firstname:{
+      required:'First name is required.',
+      minlength:'First name must be at least 2 characters long.',
+      maxlength:'First name cannot be more than 25 characters long.'
+    },
+    lastname:{
+      required:'Last name is required.',
+      minlength:'Last name must be at least 2 characters long.',
+      maxlength:'Last name cannot be more than 25 characters long.'
+    },
+    telnum:{
+      required:'Tel. Number is required.',
+      pattern:'Tel. number must contain only numbers.'
+    },
+    email:{
+      required:'Email is required.',
+      email:'Email not in valid format.'
+    }
+  };
+
   constructor(private fb:FormBuilder, private cd:ChangeDetectorRef) {
     this.createForm();
   }
@@ -24,14 +75,39 @@ export class ContactComponent implements OnInit {
 
   createForm(){
     this.feedbackForm = this.fb.group({
-      firstname:['',Validators.required],
-      lastname: ['',Validators.required],
-      telnum: [0,Validators.required],
-      email: ['',Validators.required],
+      firstname:['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0,[Validators.required,Validators.pattern]],
+      email: ['',[Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+    .subscribe(data=>{this.onValueChange(data);});
+
+    this.onValueChange();
+  }
+
+  onValueChange(data?:any){
+    if(!this.feedbackForm) return;
+    const form = this.feedbackForm;
+    for (const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){
+        // clear previous error message (if any)
+        this.formErrors[field as keyof formErros] = '';
+        const control = form.get(field);
+        if(control && control.dirty && !control.valid){
+          const messages = this.validationMessages[field as keyof validationMessages];
+          for( const key in control.errors){
+            if(control.errors.hasOwnProperty(key)){
+              this.formErrors[field as keyof formErros] += messages[key as keyof propertiesMessages]+' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   onSubmit(){
